9b3ab3941b0b976648f233e6402b3453bc653364
MuhammadAsghar1999 <143824430+MuhammadAsghar1999@users.noreply.github.com>
2023-09-11 00:08:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,118 @@
+// chapter no:06
+
+// // Q:1
+x = x + 1;
+x++;
+
+// // Q:2
+
+x--;
+
+// Q:3
+var x = 50;
+var y = x++;
+y=50
+
+// // Q:4
+var y = 50;
+var z = --y;
+z=49
+// // Q:5
+var newNum = num--;
+// // Q:6
+var originalValue = 5;
+var newValue = originalValue + 1;
+originalValue = 6
+// // Q:7
+var myNumber = 10; 
+myNumber++;         
+alert(myNumber) = 11;
+
+
+// // chapter no:07
+// // Q:1
+var calculatedNum = 2 + (2 * 6);
+calculatedNum=14;
+// // Q:2
+var calculatedNum = (2 + 2) * 6;
+calculatedNum=24;
+// // Q:3
+var calculatedNum = (2 + 2) * (4 + 2);
+calculatedNum=24;
+// // Q:4
+var calculatedNum = ((2 + 2) * 4) + 2;
+calculatedNum=18;
+// // Q:5
+var cost = (2 + 2) * (4 + 10);
+cost =56
+
+// // Q:6
+var units = 2 + (2 * 4) + 10;
+unit=20
+// Q:7
+pressure = (4 / 2) * 4
+pressure=5
+
+// // chapter no:08
+// // Q:1
+var num = "2" + "2"
+num="22"
+// // Q:2
+alert("Hello+Dolly");
+// Q:3
+alert("33" + 3);
+// alert("333")
+// // Q:4
+var part1 = "Pakistan";
+var part2 = " Zindabad";
+
+// alert(part1 + part2);
+// Q:5
+var result = "Hello, " + 42;
+// // Q:6
+var firstName = "Muhammad";
+var lastName = "Asghar";
+var fullnmae = "MuhammadAsghar"
+var fullName = firstName + " " + lastName;
+
+console.log(fullName); 
+
+// // chapter no:09
+// // Q:1
+var firstName = prompt("Enter first name");
+// Q:2
+var country = prompt("Country?", "China");
+// Q:3
+var yourName = prompt("Enter Your Name");
+Q:4
+var message = "Please enter your favorite color:";
+var defaultColor = "Black";
+var userColor = prompt(message, defaultColor);
+// // Q:5
+var question = "What's your favorite movie?";
+var defaultAnswer = "hello karachi";
+var userResponse = prompt(question, defaultAnswer);
+// // Q:6
+var messageWithDefault = "Please enter your age:";
+var defaultAge = "24";
+var userAge = prompt(messageWithDefault, defaultAge);
+alert("You entered: " + userAge);
+
+// chapter no:10
+// Q:1
+var city = "Karachi";
+if (city === "Karachi") {
+  console.log("The City OF Lights");
+}
+// // Q:2
+if (x === y) {
+    var z = prompt("Enter the value of z");
+  }
+// //   Q:3
+var ZipCode = "10010";
+if (ZipCode === "10010") {
+  alert("Karachi");
+} else {
+  alert("Please write the correct city");
+}
+// chapter no:11
